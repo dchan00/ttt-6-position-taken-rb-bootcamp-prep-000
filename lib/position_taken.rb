c2652e28313bb653b77a0d29479577c7987ec1ad
DANIEL CHAN <dchan513@msn.com>
2017-09-29 01:58:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,9 @@
 def position_taken?(board,index)
   if(board[index]==" " || board[index]=="" || board[index]==nil)
     false
-  elsif(board[index]=="X" || board[index]=="O")
-    true
+  else
+    true  
+  #elsif(board[index]=="X" || board[index]=="O")
+  #  true
   end
 end
